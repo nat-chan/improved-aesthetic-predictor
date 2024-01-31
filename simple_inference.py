0ab74362113fec0cc6080fc15d4e10f417197c70
k-sugawara <k_sugawara@radius5.co.jp>
2024-01-31 13:27:48 +0900

    外部からimportされるように再利用できないもののみを__name __ == "__main__"ステートに閉じ込める

--- simple_inference.py
+++ simple_inference.py
@@ -64,47 +64,45 @@ def normalized(a, axis=-1, order=2):
     l2[l2 == 0] = 1
     return a / np.expand_dims(l2, axis)
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--img", type=str, default="", help="単一の画像を指定する場合はこのオプションを用いる。複数の画像を処理する場合は標準入力から読み込む")
+    parser.add_argument("--batchsize", type=int, default=1, help="モデルに入力する際のバッチサイズ")
+    parser.add_argument("--both", type=bool, default=False, help="スコアと入力ファイルパスの両方を表示するか")
+    args = parser.parse_args()
 
-model = MLP(768)  # CLIP embedding dim is 768 for CLIP ViT L 14
+    model = MLP(768)  # CLIP embedding dim is 768 for CLIP ViT L 14
+    s = torch.load("sac+logos+ava1-l14-linearMSE.pth")   # load the model you trained previously or the model available in this repo
+    model.load_state_dict(s)
+    model.to("cuda")
+    model.eval()
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model2, preprocess = clip.load("ViT-L/14", device=device)  #RN50x64   
 
-s = torch.load("sac+logos+ava1-l14-linearMSE.pth")   # load the model you trained previously or the model available in this repo
-
-model.load_state_dict(s)
-
-model.to("cuda")
-model.eval()
-
-
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model2, preprocess = clip.load("ViT-L/14", device=device)  #RN50x64   
-
-#####  This script will predict the aesthetic score for this image file:
-parser = argparse.ArgumentParser()
-parser.add_argument("--img", type=str, default="", help="単一の画像を指定する場合はこのオプションを用いる。複数の画像を処理する場合は標準入力から読み込む")
-parser.add_argument("--batchsize", type=int, default=1, help="モデルに入力する際のバッチサイズ")
-parser.add_argument("--both", type=bool, default=False, help="スコアと入力ファイルパスの両方を表示するか")
-args = parser.parse_args()
-if args.img == "":
-    img_paths = [line.strip() for line in sys.stdin]
-elif args.img == "dump":
-    img_paths, dst_paths = zip(*[line.strip().split() for line in sys.stdin])
-else:
-    img_paths = [args.img]
-assert len(img_paths)%args.batchsize == 0
-pbar = tqdm(total=len(img_paths), dynamic_ncols=True)
-for j in range(len(img_paths) // args.batchsize):
-    image = torch.cat([preprocess(Image.open(img_path)).unsqueeze(0).to(device) for img_path in img_paths[j * args.batchsize:(j + 1) * args.batchsize]])
-    with torch.no_grad():
-        image_features = model2.encode_image(image)
-    im_emb_arr = normalized(image_features.cpu().detach().numpy() )
-    input_tensor = torch.from_numpy(im_emb_arr).to(device).type(torch.cuda.FloatTensor)
-    prediction = model(input_tensor)
-    for i in range(args.batchsize):
-        if args.img == "dump":
-            Path(dst_paths[j*args.batchsize+i]).write_text(str(float(prediction[i])))
-        else:
-            if args.both:
-                print( float(prediction[i]), img_paths[j*args.batchsize+i])
+    if args.img == "":
+        img_paths = [line.strip() for line in sys.stdin]
+    elif args.img == "dump":
+        img_paths, dst_paths = zip(*[line.strip().split() for line in sys.stdin])
+    else:
+        img_paths = [args.img]
+    assert len(img_paths)%args.batchsize == 0
+    pbar = tqdm(total=len(img_paths), dynamic_ncols=True)
+    for j in range(len(img_paths) // args.batchsize):
+        image = torch.cat([preprocess(Image.open(img_path)).unsqueeze(0).to(device) for img_path in img_paths[j * args.batchsize:(j + 1) * args.batchsize]])
+        with torch.no_grad():
+            image_features = model2.encode_image(image)
+        im_emb_arr = normalized(image_features.cpu().detach().numpy() )
+        input_tensor = torch.from_numpy(im_emb_arr).to(device).type(torch.cuda.FloatTensor)
+        prediction = model(input_tensor)
+        for i in range(args.batchsize):
+            if args.img == "dump":
+                Path(dst_paths[j*args.batchsize+i]).write_text(str(float(prediction[i])))
             else:
-                print( float(prediction[i]) )
-    pbar.update(n=args.batchsize)
+                if args.both:
+                    print( float(prediction[i]), img_paths[j*args.batchsize+i] )
+                else:
+                    print( float(prediction[i]) )
+        pbar.update(n=args.batchsize)
+
+if __name__ == "__main__":
+    main()
